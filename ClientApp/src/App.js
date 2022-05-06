9d01f442f67187dbe3690f6148ebb47220e8e884
PhipeMC <phipe.mcbukkit@gmail.com>
2022-05-06 12:21:52 -0500

    Correcion de API y Front End agregado

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,6 +4,18 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
+import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
+import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
+import { IndexHome } from './components/IndexHome';
+import { Transaction } from './components/Transaction';
+import FooterMenu from './components/FooterMenu'
+import { Warehouse } from './components/Warehouse';
+
+import './custom.css'
+import { Movement } from './components/Movement';
+import { Product } from './components/Product';
+import { Category } from './components/Category';
 
 import './custom.css'
 
@@ -13,9 +25,16 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={IndexHome} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/suppliers' component={Transaction} />
+        <Route path="/warehouses" component={Warehouse}></Route>
+        <Route path="/movements" component={Movement}></Route>
+        <Route path="/products" component={Product}></Route>
+        <Route path="/categorys" component={Category}></Route>
+        <AuthorizeRoute path='/fetch-data' component={FetchData} />
+        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+        <FooterMenu></FooterMenu>
       </Layout>
     );
   }
